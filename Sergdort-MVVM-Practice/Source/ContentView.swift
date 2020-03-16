691efe3b2f79a78b78975883197f10f9fdca620b
Ryuichi Fukami <fukaryu421@gmail.com>
2020-03-17 01:02:14 +0900

    first commit

---
+++ Sergdort-MVVM-Practice/Source/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sergdort-MVVM-Practice
+//
+//  Created by 深見龍一 on 2020/03/17.
+//  Copyright © 2020 深見龍一. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sergdort-MVVM-PracticeTests/Sergdort_MVVM_PracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sergdort_MVVM_PracticeTests.swift
+//  Sergdort-MVVM-PracticeTests
+//
+//  Created by 深見龍一 on 2020/03/17.
+//  Copyright © 2020 深見龍一. All rights reserved.
+//
+
+import XCTest
+@testable import Sergdort_MVVM_Practice
+
+class Sergdort_MVVM_PracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
