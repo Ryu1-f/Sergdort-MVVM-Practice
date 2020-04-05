d5593cba09e50e35161daadae272e16b9f676050
Ryuichi Fukami <fukaryu421@gmail.com>
2020-04-05 22:52:35 +0900

    add just sample view

---
+++ Sergdort-MVVM-Practice/Source/Components/Sample/SampleViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SampleViewController.swift
+//  Sergdort-MVVM-Practice
+//
+//  Created by 深見龍一 on 2020/04/02.
+//
+
+import UIKit
+import Foundation
+import RxSwift
+import RxCocoa
+
+class SampleViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.image = UIImage(named: "disney")?.rounded(to: 40)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
